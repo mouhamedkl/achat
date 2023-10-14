fb187120e77a3dbe2d4606711fb540b9f9f75b67
ademBoujnah <adem.boujnah@esprit.tn>
2023-10-14 15:03:31 +0100

    test deletion

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
@@ -1,45 +1,7 @@
 package tn.esprit.rh.achat.services;
 
-import org.junit.jupiter.api.*;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
-import tn.esprit.rh.achat.entities.Produit;
-import tn.esprit.rh.achat.repositories.ProduitRepository;
 
-import java.util.ArrayList;
-import java.util.List;
 
-@SpringBootTest
-@ExtendWith(MockitoExtension.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProduitServiceTest {
 
-    @InjectMocks
-    private ProduitServiceImpl produitService;
-
-    @Mock
-    private ProduitRepository produitRepository;
-
-    @Test
-    @Order(1)
-    public void testRetreiveAllUsers() {
-        // Créez un mock de la méthode retrieveAllProduits
-        List<Produit> produits = new ArrayList<>();
-
-        // Définissez le comportement du mock lorsque la méthode retrieveAllProduits est appelée
-        Mockito.when(produitRepository.findAll()).thenReturn(produits);
-
-        // Appelez la méthode de service que vous testez
-        List<Produit> listProduits = produitService.retrieveAllProduits();
-
-        // Assurez-vous que la méthode du service a été appelée
-        Mockito.verify(produitRepository).findAll();
-
-        // Effectuez votre assertion
-        Assertions.assertEquals(0, listProduits.size());
-    }
 }
