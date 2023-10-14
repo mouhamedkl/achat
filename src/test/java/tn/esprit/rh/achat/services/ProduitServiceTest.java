605c5a689e57218d576b7a54cca47255f7151c53
ademBoujnah <adem.boujnah@esprit.tn>
2023-10-14 14:55:35 +0100

    add 1st test mockito

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
@@ -1,24 +1,45 @@
 package tn.esprit.rh.achat.services;
-import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.rh.achat.entities.Produit;
 
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.repositories.ProduitRepository;
+
+import java.util.ArrayList;
 import java.util.List;
 
-
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProduitServiceTest {
 
-    @Autowired
-    IProduitService ps;
+    @InjectMocks
+    private ProduitServiceImpl produitService;
 
+    @Mock
+    private ProduitRepository produitRepository;
 
     @Test
     @Order(1)
-    public void testRetreiveAllUsers(){
-        List<Produit> listProduits = ps.retrieveAllProduits();
-        Assertions.assertEquals(0,listProduits.size());
+    public void testRetreiveAllUsers() {
+        // Créez un mock de la méthode retrieveAllProduits
+        List<Produit> produits = new ArrayList<>();
+
+        // Définissez le comportement du mock lorsque la méthode retrieveAllProduits est appelée
+        Mockito.when(produitRepository.findAll()).thenReturn(produits);
+
+        // Appelez la méthode de service que vous testez
+        List<Produit> listProduits = produitService.retrieveAllProduits();
+
+        // Assurez-vous que la méthode du service a été appelée
+        Mockito.verify(produitRepository).findAll();
+
+        // Effectuez votre assertion
+        Assertions.assertEquals(0, listProduits.size());
     }
 }
