181e1535920b02f0ab0320d398d3224f480ce92c
ademBoujnah <93093719+ademBoujnah@users.noreply.github.com>
2023-10-30 16:49:59 +0100

    Update Produit.java

--- src/main/java/tn/esprit/rh/achat/entities/Produit.java
+++ src/main/java/tn/esprit/rh/achat/entities/Produit.java
@@ -49,7 +49,14 @@ public class Produit implements Serializable {
 	private CategorieProduit categorieProduit;
 	
 
-
+public Produit(Long idProduit, String codeProduit, String libelleProduit, float prix, Date dateCreation, Date dateDerniereModification, Stock stock, Set<DetailFacture> detailFacture, CategorieProduit categorieProduit) {
+    this.idProduit = idProduit;
+    this.codeProduit = codeProduit;
+    this.libelleProduit = libelleProduit;
+    this.prix = prix;
+    this.dateCreation = dateCreation;
+    this.dateDerniereModification = dateDerniereModification;
+}
 	
 
 }
