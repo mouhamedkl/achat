a7201780b33ad0e6bf311410e1c6de3a17e0ba79
ademBoujnah <93093719+ademBoujnah@users.noreply.github.com>
2023-10-30 17:55:43 +0100

    Update ProduitServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
@@ -1,7 +1,8 @@
 package tn.esprit.rh.achat.services;
 
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,66 +13,54 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.esprit.examen.entities.Produit;
-import com.esprit.examen.repositories.ProduitRepository;
-import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.repositories.ProduitRepository;
 
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProduitServiceImplTest {
-	@Autowired
-	IProduitService  ProduitServicee;
-	@Mock 
-	ProduitRepository ProduitRepo;
-	
-	@InjectMocks 
-	ProduitServiceImpl ProduitService;
-	
-	 @Before
-	    public void setup() {
-	        MockitoAnnotations.initMocks(this);
-	    }
-	
-	@Test
-	public void testAddProduit() {
-	//	List<Stock> stocks = stockService.retrieveAllStocks();
-	//	int expected=stocks.size();
-		Produit s = new Produit("produit test",10,"100f");
-		Produit savedStock= ProduitService.addProduit(s);
-		
-	//	assertEquals(expected+1, stockService.retrieveAllStocks().size());
-		assertNotNull(savedStock.getLibelleProduit());
-		ProduitService.deleteProduit(savedStock.getIdProduit());
-		
-	} 
-	
-	 @Test
-	    public void testGetAllProduit() {
-	        List<Produit> toDoList = new ArrayList<Produit>();
-	        toDoList.add(new Produit("produit test2",101,"100f7"));
-	        toDoList.add(new Produit("produit test3",102,"100f6"));
-	        toDoList.add(new Produit("produit test4",104,"100fd"));
-	        when(ProduitRepo.findAll()).thenReturn(toDoList);
 
-	        List<Produit> result = ProduitService.retrieveAllProduits();
-	        assertEquals(3, result.size());
-	    }
-	 
-	 @Test
-		public void testDeleteStock() {
-		 Produit s = new Produit("produit test2",101,"100f7");
-		 Produit savedStock= ProduitService.addProduit(s);
-		 ProduitService.deleteProduit(savedStock.getIdProduit());
-			assertNull(ProduitService.retrieveProduit(savedStock.getIdProduit()));
-		}
+    @Mock
+    ProduitRepository ProduitRepo;
 
-	
-	
+    @InjectMocks
+    ProduitServiceImpl ProduitService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testAddProduit() {
+        Produit s = new Produit("produit test", 10, "100f");
+        Produit savedProduit = ProduitService.addProduit(s);
+        assertNotNull(savedProduit.getLibelleProduit());
+        ProduitService.deleteProduit(savedProduit.getIdProduit());
+    }
+
+    @Test
+    public void testGetAllProduit() {
+        List<Produit> toDoList = new ArrayList<Produit>();
+        toDoList.add(new Produit("produit test2", 101, "100f7"));
+        toDoList.add(new Produit("produit test3", 102, "100f6"));
+        toDoList.add(new Produit("produit test4", 104, "100fd"));
+        when(ProduitRepo.findAll()).thenReturn(toDoList);
+
+        List<Produit> result = ProduitService.retrieveAllProduits();
+        assertEquals(3, result.size());
+    }
+
+    @Test
+    public void testDeleteProduit() {
+        Produit s = new Produit("produit test2", 101, "100f7");
+        Produit savedProduit = ProduitService.addProduit(s);
+        ProduitService.deleteProduit(savedProduit.getIdProduit());
+        assertNull(ProduitService.retrieveProduit(savedProduit.getIdProduit()));
+    }
 }
